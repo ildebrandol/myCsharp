f733dea85766adb47f54bc737bfcb78e0e1a31ec
Daria Chystiakova <daria_chystiakova@epam.com>
2018-01-03 18:21:50 +0200

    changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,26 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        class Flower
+        {
+            public string name;
+            public Flower(string _name)
+            {
+                name = _name;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public static int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
         static void Main(string[] args)
         {
             int[] array = new int[] { 1, 2, 3, 4, 5 };
@@ -27,6 +47,11 @@ namespace ConsoleApp1
             {
                 Console.WriteLine(a);
             }
+
+            Flower rose = new Flower("flower1");
+            Console.WriteLine(rose.GetName());
+
+            Console.WriteLine(Flower.Sum(1, 2));
             Console.ReadLine();
         }
     }
